2fdf6c218a0138ce0adbdc865578afecdc924dfe
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2023-03-13 21:08:38 -0300

    Utilizar expressões para estilizar elementos com ngClass

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [/*
+  listaPensamentos = [
     {
       conteudo: 'Passo informações para o componente filho',
       autoria: 'Componente Pai',
@@ -17,7 +17,12 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'Minha propriedade é decorada com @Input()',
       autoria: 'Componente Filho',
       modelo: 'modelo3'
-    }*/
+    },
+    {
+      conteudo: `Os Estados-Nações, ou propriamente dito países, surgiram principalmente no fim do século XVIII início do século XIX. Foram constituídos a partir do processo de industrialização original e/ou clássica com mecanismo de divisão do espaço geográfico internacional, estabelecendo uma nova configuração política e espacial, tudo isso é fruto da burguesia e revolução industrial que contribuiu para proteger o mercado de um determinado território`,
+      autoria: 'Componente',
+      modelo: 'modelo1'
+    }
   ];
 
 
