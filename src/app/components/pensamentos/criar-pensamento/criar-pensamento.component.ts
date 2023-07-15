232e39823991ec8c27e5947041d7187cb93b11b0
David Teixeira <67128503+davidasteixeira@users.noreply.github.com>
2023-07-14 22:44:47 -0300

    Transformando o formulário em reativo

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -12,26 +13,54 @@ export class CriarPensamentoComponent implements OnInit {
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo:'modelo1'
-  }
+  formulario!: FormGroup;
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/)
+        ])],
+      autoria: ['',Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          this.minusculoValidator
+        ])],
+      modelo: ['modelo1']
+    })
+  }
+
+  habilitarBotao(): String {
+    if(this.formulario.valid){
+      return 'botao'
+    }else{
+      return 'botao__desabilitado'
+    }
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(()=>{
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors)
+    if(this.formulario.valid){
+      this.service.criar(this.formulario.value).subscribe(()=>{
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
   }
 
   cancelarPensamento() {
     this.router.navigate(['/listarPensamento'])
+  }
+
+  minusculoValidator(control: AbstractControl){
+    const autoria = control.value as string;
+    if(autoria !== autoria?.toLocaleLowerCase()){
+      return {minusculo:true};
+    }else
+      return null;
   }
 
 }
